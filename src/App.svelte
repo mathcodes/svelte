a26e43cbe3a0ed7cc2eae8648b311e12ad11e712
Jon Christie <jonpchristie@gmail.com>
2022-06-30 12:23:06 -0600

    svelte initial

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	let firstName = 'Jon';
+	let lastName = 'Christie';
+	let color = '#d35400'
+
+	$: name = firstName + ' ' + lastName
+
+	const toggle = ()=> {
+		color = color == 'blue' ? 'red' : 'blue'
+	}
+</script>
+
+<!-- OUTPUT / HTML  -->
+<main>
+	<h1 style="color: {color}">Hello {name}!</h1>
+
+	<!-- <button on:click={()=> color = 'purple'}>CLICK ME </button> -->
+	<button on:click={toggle}>CLICK ME </button>
+</main>
+
+<!-- STYLE TAGS  -->
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
